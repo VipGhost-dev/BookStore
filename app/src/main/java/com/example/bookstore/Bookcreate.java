5b43c1259fa07ccb6b88d09cb0bcff100c8068e5
Vip_Ghost <lied00@mail.ru>
2022-04-29 00:29:55 +0300

    Six commit

---
+++ app/src/main/java/com/example/bookstore/Bookcreate.java
@@ -0,0 +1,76 @@
+package com.example.bookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Bookcreate extends AppCompatActivity implements View.OnClickListener {
+
+    Button btncreate;
+    EditText dbName, dbAuthor, dbPrice;
+
+    DB db;
+    SQLiteDatabase database;
+    ContentValues contentValues;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookcreate);
+
+        btncreate = findViewById(R.id.btncreate);
+        btncreate.setOnClickListener(this);
+
+        dbName = findViewById(R.id.dbName);
+        dbAuthor = findViewById(R.id.dbAuthor);
+        dbPrice = findViewById(R.id.dbPrice);
+
+        db = new DB(this);
+        database = db.getWritableDatabase();
+        dbName.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus)
+                dbName.setHint("");
+            else
+                dbName.setHint("Name");
+        });
+        dbAuthor.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus)
+                dbAuthor.setHint("");
+            else
+                dbAuthor.setHint("Author");
+        });
+        dbPrice.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus)
+                dbPrice.setHint("");
+            else
+                dbPrice.setHint("Price");
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btncreate:
+                String name = dbName.getText().toString();
+                String author = dbAuthor.getText().toString();
+                String price = dbPrice.getText().toString();
+                contentValues = new ContentValues();
+                contentValues.put(DB.KEY_NAME, name);
+                contentValues.put(DB.KEY_AUTHOR, author);
+                contentValues.put(DB.KEY_PRICE, price);
+
+                database.insert(DB.TABLE_BOOKS, null, contentValues);
+                dbName.setText("");
+                dbAuthor.setText("");
+                dbPrice.setText("");
+                startActivity(new Intent(this, bookspage.class));
+                break;
+        }
+    }
+}
